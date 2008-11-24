561bacc3ffa2d8903e61e2535aa961c2c8d6b951
Mads Chr. Olesen <shiyee@shiyee.dk>
2008-11-24 10:44:20 +0100

    Add Arild to author list

--- pyuppaal.py
+++ pyuppaal.py
@@ -3,6 +3,7 @@
 """
     Copyright (C) 2008 Mads Christian Olesen <mchro@cs.aau.dk>
                        Andreas Engelbredt Dalsgaard <andreas.dalsgaard@gmail.com>
+                       Arild Martin Møller Haugstad <arild@cs.aau.dk>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
