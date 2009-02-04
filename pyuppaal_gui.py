a5aaea483248c75a37d62e63fdc3c12fa2d3321a
Andreas Engelbredt Dalsgaard <andreas@metope>
2009-02-04 17:09:32 +0100

    Added license and license notice in pyuppaal_gui.py

--- pyuppaal_gui.py
+++ pyuppaal_gui.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
+
+"""
+    This program is a graphical user interface for pyuppaal. It uses goocanvas and 
+    was initially inspired from examples included in the python bindings of goocanvas.
+
+    Copyright (C) 2008 Andreas Engelbredt Dalsgaard <andreas.dalsgaard@gmail.com>
+                       Mads Christian Olesen <mchro@cs.aau.dk>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. """
+
+
 from pyuppaal import *
 import goocanvas
 import gtk
