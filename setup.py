93df3899b46b65742ca98aa9b20c2522ba852059
Mads Chr. Olesen <launchpad@shiyee.dk>
2010-02-16 12:28:18 +0100

    Initial setuptools and debian packaging

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+### BEGIN LICENSE
+# Copyright (C) 2010 Mads Chr. Olesen <mchro@cs.aau.dk>
+#This program is free software: you can redistribute it and/or modify it 
+#under the terms of the GNU General Public License version 3, as published 
+#by the Free Software Foundation.
+#
+#This program is distributed in the hope that it will be useful, but 
+#WITHOUT ANY WARRANTY; without even the implied warranties of 
+#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#PURPOSE.  See the GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along 
+#with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
+
+try:
+    import DistUtilsExtra.auto
+except ImportError:
+    import sys
+    print >> sys.stderr, 'To build pyuppaal you need https://launchpad.net/python-distutils-extra'
+    sys.exit(1)
+
+assert DistUtilsExtra.auto.__version__ >= '2.10', 'needs DistUtilsExtra.auto >= 2.10'
+import os
+
+        
+##################################################################################
+###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
+##################################################################################
+
+DistUtilsExtra.auto.setup(
+    name='pyuppaal',
+    version='0.1',
+    license='GPL-3',
+    author='Mads Chr. Olesen',
+    author_email='mchro@cs.aau.dk',
+    description='Python library for manipulating UPPAAL xml files',
+    #long_description='Here a longer description',
+    url='https://launchpad.net/pyuppaal'
+    )
+
